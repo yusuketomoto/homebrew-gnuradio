9495f8b600dd295cbb77aa7bc2faeb1d6adb3e44
TT <edy555@gmail.com>
2014-01-07 22:06:41 +0900

    add dependency to autoconf

--- rtlsdr.rb
+++ rtlsdr.rb
@@ -5,14 +5,15 @@ class Rtlsdr < Formula
   head 'git://git.osmocom.org/rtl-sdr.git'
 
   depends_on 'pkg-config' => :build
+  depends_on 'autoconf' => :build
   depends_on 'automake' => :build
   depends_on 'libtool' => :build
   depends_on 'cmake' => :build
-  depends_on 'libusb'
+  depends_on 'libusbx'
 
   def install
     args = ["--prefix=#{prefix}"]
-    system "autoreconf -i"
+    system "autoreconf", "-fi" if build.head?
     system "./configure", *args
     system "make install"
   end
